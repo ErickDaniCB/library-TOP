4bba1ffc6e5b937927279198a1f16ec3d78621c8
ErickDaniCB <erickdani10@hotmail.com>
2024-10-09 16:44:45 -0600

    Add DOM manipulation to show books' cards

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 // index.js
 import "./styles.css";
 
-let library = [
+let libraryArr = [
   {
-    name: "The Final Empire",
+    title: "The Final Empire",
     author: "Brandon Sanderson",
     pages: 666,
     read: true,
-  }
+  },
 ];
 
-function bookConstructor(name, author, pages, read) {
+function bookConstructor(title, author, pages, read) {
   return {
-    name,
+    title,
     author,
     pages,
     read,
   };
 }
 
-function addBook(name, author, pages, read) {
-  library.push(bookConstructor(name, author, pages, read));
+function addBook(title, author, pages, read) {
+  libraryArr.push(bookConstructor(title, author, pages, read));
 }
 
 // DISPLAY DIALOG FORM
@@ -28,12 +28,11 @@ function addBook(name, author, pages, read) {
 const openBtn = document.getElementById("open-dialog");
 const dialog = document.getElementById("dialog");
 
-
 openBtn.addEventListener("click", () => {
   dialog.showModal();
 });
 
-// GET FORM INFO AND ADD IT TO LIBRARY[]  
+// GET FORM INFO AND ADD IT TO libraryArr[]
 
 const title = document.getElementById("title");
 const author = document.getElementById("author");
@@ -41,6 +40,38 @@ const pages = document.getElementById("pages");
 const read = document.getElementById("read");
 const addBtn = document.getElementById("add");
 
-addBtn.addEventListener("click", (event) => {
-  addBook(title.value, author.value, pages.value, read.checked)
+addBtn.addEventListener("click", () => {
+  addBook(title.value, author.value, pages.value, read.checked);
+  updateLibrary();
 });
+
+// CREATE AND DISPLAY CARDS
+
+const libraryDiv = document.getElementById("library");
+
+function createCard(obj) {
+  const card = document.createElement("div");
+  card.setAttribute("class", "card");
+  const cardTitle = document.createElement("h3");
+  cardTitle.textContent = obj.title;
+  const cardAuthor = document.createElement("h3");
+  cardAuthor.textContent = obj.author;
+  const cardPages = document.createElement("h3");
+  cardPages.textContent = obj.pages;
+  libraryDiv.appendChild(card);
+  card.appendChild(cardTitle);
+  card.appendChild(cardAuthor);
+  card.appendChild(cardPages);
+}
+
+// UPDATE LIBRARY DISPLAY   
+function updateLibrary (){ 
+  while (libraryDiv.firstChild){
+    libraryDiv.removeChild(libraryDiv.firstChild)
+  }
+  libraryArr.forEach((bookObj) => {
+    createCard(bookObj);
+  });
+}
+
+updateLibrary();
