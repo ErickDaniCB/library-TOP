1622fa17bba99da969839287f206c1f11e07ed38
ErickDaniCB <erickdani10@hotmail.com>
2024-10-09 13:27:42 -0600

    Add book constructor and add function, and dialog

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 // index.js
 import "./styles.css";
-import { greeting } from "./greeting.js";
 
-console.log(greeting);
+let library = [
+  {
+    name: "The Final Empire",
+    author: "Brandon Sanderson",
+    pages: 666,
+    read: true,
+  },
+];
+
+function bookConstructor(name, author, pages, read) {
+  return {
+    name,
+    author,
+    pages,
+    read,
+  };
+}
+
+// console.log(bookConstructor("aaaa", "oooo", 233, false));
+
+function addBook(name, author, pages, read) {
+  library.push(bookConstructor(name, author, pages, read));
+}
+
+// DIALOG 
+
+const add = document.getElementById("add");
+const dialog = document.getElementById("dialog");
+
+add.addEventListener("click", () => {dialog.showModal()})
